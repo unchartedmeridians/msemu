c7e232112af4db41982959e58475c93f50b75043
ben <afamiliarnoise@gmail.com>
2016-10-20 20:01:01 +0100

    Changed these so theyll actually build

--- emulator/io.c
+++ emulator/io.c
@@ -1,8 +1,9 @@
 #include <stdint.h>
 #include "io.h"
 
-uint8_t io_port_read(uint8_t port) 
+uint8_t io_port_read(uint8_t port)
 {
+    //TODO
     return 0xFF;
 }
 
--- emulator/vdp.c
+++ emulator/vdp.c
@@ -5,7 +5,6 @@ uint8_t cram[0x32] = { 0 };
 
 struct Vdp {
     uint8_t reg_control;
-    
     uint8_t reg_mode_control1;
     uint8_t reg_mode_control2;
     uint8_t reg_name_table_base_addr;
@@ -17,3 +16,4 @@ struct Vdp {
     uint8_t bg_x_scroll;
     uint8_t bg_y_scroll;
     uint8_t line_counter;
+};
